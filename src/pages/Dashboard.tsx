340f08b93385537c6c20fa9eb3939e8089a59a16
MatoG50 <matog50@gmail.com>
2026-01-24 13:39:18 +0300

    setup dasiyui and tailwind

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+const Dashboard = () => {
+  return (
+    <>
+      <div className="mt-4">Welcome to Dashboard </div>
+
+      <button className="btn btn-primary">Click</button>
+    </>
+  );
+};
+
+export default Dashboard;
