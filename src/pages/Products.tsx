d0208e855cdf606ac7e19f770f7b3c84d7db5f78
MatoG50 <matog50@gmail.com>
2026-02-01 15:24:16 +0300

    connected backend and frontend

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,7 +1,48 @@
+import axios from "axios";
 import CartPanel from "../components/Cart";
 import ProductCard from "../components/ProductCard";
+import { useEffect, useState } from "react";
+import type { CartItem, Product } from "../entities/types";
+
+interface ApiResponse {
+  status: number;
+  message: string;
+  data: Product[];
+}
 
 const Products = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [cart, setCart] = useState<CartItem[]>([]);
+
+  const handleAddToCart = (product: Product) => {
+    setCart(prev => {
+      const exists = prev.find(item => item.product.id === product.id);
+      if (exists) {
+        return prev.map(item =>
+          item.product.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        );
+      }
+      return [...prev, { product, quantity: 1 }];
+    });
+  };
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await axios.get<ApiResponse>(
+          "http://localhost:5001/api/products",
+        );
+
+        setProducts(res.data.data);
+      } catch (err) {
+        console.error(err);
+        alert("Failed to fetch products");
+      }
+    };
+    fetchProducts();
+  }, []);
   return (
     <div className="h-full flex">
       <div className="flex-1 p-6 space-y-4">
@@ -28,18 +69,17 @@ const Products = () => {
         </div>
 
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {products.map(prod => (
+            <ProductCard
+              key={prod.id}
+              product={prod}
+              onAddToCart={handleAddToCart}
+            />
+          ))}
         </div>
       </div>
 
-      <CartPanel />
+      <CartPanel cart={cart} setCart={setCart} />
     </div>
   );
 };
